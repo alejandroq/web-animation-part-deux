e0d45c5ce880a9147c7deecf68b0c06862412088
Heinrichs, Jeff <jeff.heinrichs@icf.com>
2017-09-28 10:43:34 -0400

    adding files

---
+++ src/app/pages/plugins-page/plugins-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plugins-page',
+  templateUrl: './plugins-page.component.html',
+  styleUrls: ['./plugins-page.component.css']
+})
+export class PluginsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
